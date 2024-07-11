9837beb702c53a328977299a43130483165dbbc1
Krypto <justin.wloka26@gmail.com>
2024-07-11 12:04:07 +0200

    combat v1

--- boss.py
+++ boss.py
@@ -1,9 +1,9 @@
 class Boss():
     def __init__(self, name, hp, atk, defense, level):
         self.name = name
-        self.hp = hp
-        self.atk = atk
-        self.defense = defense
+        self.hp = hp + (90 * level)
+        self.atk = atk + (9 * level)
+        self.defense = defense + (6 * level)
         self.level = level
 
     def get_name(self):
@@ -13,11 +13,14 @@ class Boss():
         return self.hp
 
     def set_hp(self, hp):
-        pass
+        self.hp = hp
 
 
     def get_attack(self):
         return self.atk
+
+    def set_attack(self, atk):
+        self.atk = atk
 
     def get_defense(self):
         return self.defense
@@ -30,12 +33,13 @@ class Boss():
 
 
 class Knight():
-    def __init__(self, name, hp, atk, defense, level):
+    def __init__(self, name, hp, atk, defense, level, xp):
         self.name = name
-        self.hp = hp
-        self.atk = atk
-        self.defense = defense
+        self.hp = hp + (75 * level)
+        self.atk = atk + (7 * level)
+        self.defense = defense + (4 * level)
         self.level = level
+        self.xp = xp
 
     def get_name(self):
         return self.name
@@ -54,6 +58,12 @@ class Knight():
 
     def get_level(self):
         return self.level
+
+    def get_xp(self):
+        return self.xp
+
+    def set_xp(self, xp):
+        self.xp = xp
 
     def kill(self):
         pass
--- main.py
+++ main.py
@@ -1,13 +1,65 @@
+import random
 from boss import Boss
 from boss import Knight
 
 Turn = 0
+CharName = input("Enter your character name: ")
 Boss1 = Boss("Goblin", 100, 5, 5, 1)
-Knight1 = Knight("Arthur", 100, 10, 3, 1)
-print(f"{'':>80}Turn {Turn} \n{'Boss':<15}{'HP':<15}{'Attack':<15}{'Defense':<15}{'Level':<15}{'':>25}{'Boss':<15}"
-      f"{'HP':<15}{'Attack':<15}{'Defense':<15}{'Level':<15}")
-print(f"{Knight1.get_name():<15}{Knight1.get_hp():<15}{Knight1.get_attack():<15}{Knight1.get_defense():<15}{Knight1.get_level():<15}"
-      f"{'':>25}{Boss1.get_name():<15}{Boss1.get_hp():<15}{Boss1.get_attack():<15}{Boss1.get_defense():<15}{Boss1.get_level():<15}")
+Knight1 = Knight(CharName, 100, 10, 3, 2, 0)
 
+Base_Boss_HP = Boss1.get_hp()
+Base_Boss_ATK = Boss1.get_attack()
+Base_Boss_DEF = Boss1.get_defense()
+Base_Boss_LVL = Boss1.get_level()
+Base_Knight_HP = Knight1.get_hp()
+Base_Knight_ATK = Knight1.get_attack()
+Base_Knight_DEF = Knight1.get_defense()
+Base_Knight_LVL = Knight1.get_level()
+Player_XP_Thr = (75 + (100 * Knight1.get_level())) * 1.5
+Kill_Count = 0
+Boss_Choices = "Attack", "Defend", "Sleep"
 
+while Knight1.get_hp() >= 0: #and Boss1.get_hp() >= 0:
+    Turn += 1
+    print(f"{'':>80}Turn {Turn} \n{'Boss':<15}{'HP':<15}{'Attack':<15}{'Defense':<15}{'Level':<15}{'':>25}{'Boss':<15}"
+          f"{'HP':<15}{'Attack':<15}{'Defense':<15}{'Level':<15}")
+    print(f"{Knight1.get_name():<15}{Knight1.get_hp():<15}{Knight1.get_attack():<15}{Knight1.get_defense():<15}{Knight1.get_level():<15}"
+          f"{'':>25}{Boss1.get_name():<15}{Boss1.get_hp():<15}{Boss1.get_attack():<15}{Boss1.get_defense():<15}{Boss1.get_level():<15}")
 
+    boss_choice = random.choice(Boss_Choices)
+    choice_att_def = input(f"\n{'':<68}What do you want to do? Attack or Defend: ")
+
+    if choice_att_def == "Attack" or choice_att_def == "attack" or choice_att_def == "A" or choice_att_def == "a":
+        print(f"{'':<68}{Knight1.get_name()} attacks {Boss1.get_name()}")
+        if boss_choice == "Defend":
+            print(f"{'':<68}{Boss1.get_name()} defends")
+            Boss1.set_hp(Boss1.get_hp() - ((Knight1.get_attack() - Boss1.get_defense()) / 2))
+        else:
+            Boss1.set_hp(Boss1.get_hp() - (Knight1.get_attack() - Boss1.get_defense()))
+
+    elif choice_att_def == "Defend" or choice_att_def == "defend" or choice_att_def == "D" or choice_att_def == "d":
+        print(f"{'':<68}{Knight1.get_name()} defends")
+        Boss1.set_attack(Boss1.get_attack() / 2)
+
+    else:
+        heal = (Knight1.get_level() * 5) * 1.5
+        Knight1.set_hp(Knight1.get_hp() + heal)
+        print(f"{'':<68}{Knight1.get_name()} sleeps and regains {heal} HP")
+
+    if boss_choice == "Attack":
+        print(f"{'':<68}{Boss1.get_name()} attacks {Knight1.get_name()}")
+        Knight1.set_hp(Knight1.get_hp() - Boss1.get_attack())
+
+    elif boss_choice == "Sleep":
+        heal = (Boss1.get_level() * 10) * 1.5
+        Boss1.set_hp(Boss1.get_hp() + heal)
+        print(f"{'':<68}{Boss1.get_name()} sleeps and regains {heal} HP")
+        continue
+
+    if Boss1.get_hp() <= 0:
+        print(f"{'':<68}{Knight1.get_name()} has defeated {Boss1.get_name()}")
+        break
+
+    elif Knight1.get_hp() <= 0:
+        print(f"{'':<68}{Boss1.get_name()} has defeated {Knight1.get_name()}")
+        break
